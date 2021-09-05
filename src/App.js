778496fe2f4408f5b3d7f9b49ffbbe9252c2f3fe
Anant Kumawat <anantkumawat22@gmail.com>
2021-09-05 11:15:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [input, setInput] = useState(0);
+
+  // useEffect(()=>{
+  //   document.getElementById("#num").value="";
+  // },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav className="nav_bar">
+        <p>Calculator App</p>
+      </nav>
+      <div className="container">
+        <div className="center_div">
+          <input type="text" value={input} id="num" />
+          <div class="grid_div">
+            <div class="box" id="item1">AC</div>
+            <div class="box" id="item2"><i class="fas fa-backspace"></i></div>
+            <div class="box operator" id="item3">%</div>
+
+            <div class="box operator" id="item4">/</div>
+            <div class="box operand" id="item5">7</div>
+
+            <div class="box operand" id="item6">8</div>
+            <div class="box operand" id="item7">9</div>
+
+            <div class="box operator" id="item8">*</div>
+            <div class="box operand" id="item9">4</div>
+
+            <div class="box operand" id="item10">5</div>
+            <div class="box operand" id="item11">6</div>
+
+            <div class="box operator" id="item12">-</div>
+            <div class="box operand" id="item13">1</div>
+
+            <div class="box operand" id="item14">2</div>
+            <div class="box operand" id="item15">3</div>
+
+            <div class="box operator" id="item16">+</div>
+
+            <div class="box operand" id="item17" >0</div>
+            <div class="box operand" id="item18">.</div>
+
+            <div class="box b1" id="item19">=</div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
