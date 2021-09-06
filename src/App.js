dd5f19acf252088fa03e2b19a40050a81d5c375d
Anant Kumawat <anantkumawat22@gmail.com>
2021-09-06 13:33:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -6,9 +6,30 @@ function App() {
 
   const [input, setInput] = useState(0);
 
-  // useEffect(()=>{
-  //   document.getElementById("#num").value="";
-  // },[]);
+  function allClear() {
+    setInput(0);
+  }
+
+  function backspace() {
+    if (input.length >= 0) {
+      var newStr = input.substring(0, input.length - 1);
+      if (newStr == "") {
+        setInput(0);
+        return;
+      }
+      setInput(newStr);
+    }
+  }
+
+  const inputChange = () => {
+    var x = document.getElementById("num").value;
+    setInput(x);
+  }
+
+  // function Add(x, y){
+  //   return(x + y);
+  // }
+
 
   return (
     <>
@@ -17,36 +38,37 @@ function App() {
       </nav>
       <div className="container">
         <div className="center_div">
-          <input type="text" value={input} id="num" />
-          <div class="grid_div">
-            <div class="box" id="item1">AC</div>
-            <div class="box" id="item2"><i class="fas fa-backspace"></i></div>
-            <div class="box operator" id="item3">%</div>
+          <input type="text" onChange={inputChange} value={input} id="num" />
+          <div className="grid_div">
 
-            <div class="box operator" id="item4">/</div>
-            <div class="box operand" id="item5">7</div>
+            <div className="box" id="item1" onClick={allClear}>AC</div>
+            <div className="box" id="item2" onClick={backspace}><i className="fas fa-backspace"></i></div>
+            <div className="box operator" id="item3">%</div>
 
-            <div class="box operand" id="item6">8</div>
-            <div class="box operand" id="item7">9</div>
+            <div className="box operator" id="item4">/</div>
+            <div className="box operand" id="item5">7</div>
 
-            <div class="box operator" id="item8">*</div>
-            <div class="box operand" id="item9">4</div>
+            <div className="box operand" id="item6">8</div>
+            <div className="box operand" id="item7">9</div>
 
-            <div class="box operand" id="item10">5</div>
-            <div class="box operand" id="item11">6</div>
+            <div className="box operator" id="item8">*</div>
+            <div className="box operand" id="item9">4</div>
 
-            <div class="box operator" id="item12">-</div>
-            <div class="box operand" id="item13">1</div>
+            <div className="box operand" id="item10">5</div>
+            <div className="box operand" id="item11">6</div>
 
-            <div class="box operand" id="item14">2</div>
-            <div class="box operand" id="item15">3</div>
+            <div className="box operator" id="item12">-</div>
+            <div className="box operand" id="item13">1</div>
 
-            <div class="box operator" id="item16">+</div>
+            <div className="box operand" id="item14">2</div>
+            <div className="box operand" id="item15">3</div>
 
-            <div class="box operand" id="item17" >0</div>
-            <div class="box operand" id="item18">.</div>
+            <div className="box operator" id="item16">+</div>
 
-            <div class="box b1" id="item19">=</div>
+            <div className="box operand" id="item17" >0</div>
+            <div className="box operand" id="item18">.</div>
+
+            <div className="box b1" id="item19">=</div>
           </div>
         </div>
       </div>
